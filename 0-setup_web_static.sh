088cd7ac18d3a125385a7826196cc2e12f130c9d
AteCastillo <atecast@gmail.com>
2021-04-15 16:45:33 -0300

    fix 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
-# Prepare server
+# sets up your web servers for the deployment of web_static
 apt-get update
-apt-get -y install nginx
+apt-get install nginx -y
 mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+mkdir -p /data/web_static/shared/
+echo 'hola Atenea' > /data/web_static/releases/test/index.html
+ln -fs /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
 sed -i '/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n\tautoindex on;\n}\n' /etc/nginx/sites-available/default
 service nginx restart
+# apt-get update
+# apt-get -y install nginx
+# mkdir -p /data/web_static/releases/test
+# mkdir -p /data/web_static/shared
+# echo "Holberton School" > /data/web_static/releases/test/index.html
+# ln -sf /data/web_static/releases/test /data/web_static/current
+# chown -R ubuntu:ubuntu /data/
+# sed -i '/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n\tautoindex on;\n}\n' /etc/nginx/sites-available/default
+# service nginx restart
