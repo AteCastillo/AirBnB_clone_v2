bb84ff867877131fd8239b17cf54fabfa9388f51
AteCastillo <atecast@gmail.com>
2021-04-15 16:42:05 -0300

    fix 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static
+# Prepare server
 apt-get update
-if [ ! -x /usr/sbin/nginx ];
-then
-        apt-get install nginx -y
-fi
+apt-get -y install nginx
 mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared/
-echo 'hola Atenea' > /data/web_static/releases/test/index.html
-ln -fs /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n\tautoindex off;\n}\n' /etc/nginx/sites-available/default
+mkdir -p /data/web_static/shared
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n\tautoindex on;\n}\n' /etc/nginx/sites-available/default
 service nginx restart
