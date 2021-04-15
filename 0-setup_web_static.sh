3e6bc2785cc554838972bddcfb0dabcd1e4a9d3e
AteCastillo <atecast@gmail.com>
2021-04-15 16:08:15 -0300

    fix 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 apt-get update
-apt-get install nginx -y
+if [ ! -x /usr/sbin/nginx ];
+then
+        apt-get install nginx -y
+fi
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared/
 echo 'hola Atenea' > /data/web_static/releases/test/index.html
 ln -fs /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a \\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\tautoindex off;\n}\n' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n\tautoindex off;\n}\n' /etc/nginx/sites-available/default
 service nginx restart
