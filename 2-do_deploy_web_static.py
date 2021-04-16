8d1c5accbcdf22f784f7aeb4279a0a00865b3c38
AteCastillo <atecast@gmail.com>
2021-04-16 18:03:13 -0300

    fix 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,7 +10,8 @@ env.hosts = ['34.74.47.238', '35.231.230.80']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers, using the function do_deploy"""
+    """distributes an archive to your web servers,
+    using the function do_deploy"""
     if not path.exists(archive_path):
         return False
 
@@ -20,16 +21,20 @@ def do_deploy(archive_path):
     file_noextension = file0[0]
     new_tmpfile = "/tmp/{}".format(file_withextension)
     destination = "/data/web_static/releases"
-    # print('mv {}/{}/web_static/* {}/{}'.format(destination, file_noextension, destination, file_noextension))
+
     try:
         put(archive_path, new_tmpfile)
         run('mkdir -p {}/{}'.format(destination, file_noextension))
-        run('tar -xzf {} -C {}/{}'.format(new_tmpfile, destination, file_noextension))
+        run('tar -xzf {} -C {}/{}'.
+            format(new_tmpfile, destination, file_noextension))
         run('rm -f {}'.format(new_tmpfile))
-        run('mv {}/{}/web_static/* {}/{}'.format(destination, file_noextension, destination, file_noextension))
+        run('mv {}/{}/web_static/* {}/{}'.
+            format(destination, file_noextension,
+                   destination, file_noextension))
         run('rm -rf {}/{}/web_static'.format(destination, file_noextension))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}/{} /data/web_static/current'.format(destination, file_noextension))
+        run('ln -s {}/{} /data/web_static/current'.
+            format(destination, file_noextension))
         return True
     except:
         return False
