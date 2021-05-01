77944f8a9fa80036c7363d48578aac5a3bca8e19
AteCastillo <atecast@gmail.com>
2021-05-01 18:45:47 -0300

    fixing 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,60 +1,62 @@
-'''starts a Flask web application'''
-
+#!/usr/bin/python3
+'''Flask Module'''
 from flask import Flask, abort, render_template
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    '''web application must be listening on 0.0.0.0, port 5000'''
-    return "Hello HBNB!"
+def hello_world():
+    '''First Flask Function'''
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    '''new route'''
+def hello_HBNB():
+    '''Second Flask Function'''
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def text(text):
-    '''new route with variable'''
-    return 'C {}'.format(str(text.replace('_', ' ')))
+def hello_c(text):
+    '''Third Flask Function'''
+    return "C {}".format(str(text)).replace("_", " ")
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def texts(text='is cool'):
-    '''new route with text variable'''
-    return 'Python {}'.format(str(text.replace('_', ' ')))
+def hello_python(text="is cool"):
+    '''Forth Flask Function'''
+    return "Python {}".format(str(text)).replace("_", " ")
 
 
 @app.route('/number/<n>', strict_slashes=False)
-def number(n):
-    '''new route with int variable'''
+def hello_num(n):
+    '''Fifth Flask Function'''
     try:
-        return '{} is a number'.format(int(n))
+        return "{} is number".format(int(n))
     except:
-        abort(404)
+        return abort(404)
 
 
 @app.route('/number_template/<n>', strict_slashes=False)
-def number_template(n):
-    '''new route with int variable and render'''
+def num_temp_func(n):
+    """Number Page"""
     try:
-        return render_template('5-number.html', n=(int(n)))
+        number = int(n)
+        return render_template("5-number.html", number=n)
     except:
         abort(404)
 
 
 @app.route('/number_odd_or_even/<n>', strict_slashes=False)
-def odd_or_even(n):
-    '''new route with int variable and render'''
+def num_odd_even(n):
+    '''when odd or even'''
     try:
-        return render_template('6-number_odd_or_even.html', n=(int(n)))
+        number = int(n)
+        return render_template('6-number_odd_or_even.html', number=number)
     except:
         abort(404)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
